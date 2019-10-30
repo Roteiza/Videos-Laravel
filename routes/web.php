dea356db9663d8ce69fcb8536004fcc8600e05cd
Rodrigo Oteiza <roteiza@outlook.com>
2019-10-30 01:59:47 -0300

    DB y relaciones creadas

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Use App\Video;
+
+Route::get('/', function () {
+
+    $videos = Video::all();
+    foreach($videos as $video){
+        echo $video->title;
+        echo $video->user->email."<br>";
+        foreach($video->comments as $comment){
+            echo $comment->body;
+        }
+        echo "<hr>";
+    }
+
+    return view('welcome');
+});
